b19f008e3e680bb06e9164ae965c21b7b36003ce
vivianafabio <anabonovoxu22@gmail.com>
2026-05-29 21:53:06 -0300

    criado novo projeto arrayjava

--- vetores/aulavetores/src/App.java
+++ vetores/aulavetores/src/App.java
@@ -1,5 +1,13 @@
+import  java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Scanner inUser = new Scanner(System.in);
+
+    System.out.println("Digite o seu nome");
+    String nome = inUser.nextLine ();
+    System.out.printf ("o nome informado e: %s",nome);
+        inUser.close();
+}
 }
